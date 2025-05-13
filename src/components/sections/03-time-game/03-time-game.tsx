e39ef792c33fb69e257030dd8c95f2aa9d67ce56
Bence R <bencethegreat@gmail.com>
2025-05-14 00:04:21 +0100

    Added section label craic

--- src/components/sections/03-time-game/03-time-game.tsx
+++ src/components/sections/03-time-game/03-time-game.tsx
@@ -4,10 +4,19 @@ import { useSection } from "@/components/contexts/SectionContext";
 import { useInView } from "@/components/hooks/useInView";
 import { EnterGame } from "./game";
 import { PressableKeyButton } from "@/components/button/pressable-button";
+import { useEffect } from "react";
 
 export const TimeGame = () => {
+	const sectionName = "03 - HJKL Game"
+
 	const { ref, isInView } = useInView(0.5);
 	const { currentSection, setCurrentSection } = useSection()
+
+	useEffect(() => {
+		if (isInView) {
+			setCurrentSection(sectionName);
+		}
+	}, [isInView]);
 
 	return (
 		<section ref={ref} id="03-time-game" className="flex font-share h-screen w-full gap-y-12 flex-col justify-center items-center">
